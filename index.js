3bce399a4f7c65ddd66cc922720e55d4f10d5c70
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-09-07 01:05:36 -0500

    second pull

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 const multer  = require('multer')
 const upload = multer({ storage: multer.memoryStorage() })
 const Jimp = require('jimp')
-
-//https://goquotes-api.herokuapp.com/api/v1/random?count=1
+const fetch = import('node-fetch')
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname+'/src/index.html')
@@ -14,6 +13,13 @@ app.post('/stats', upload.single('uploaded_file'), function (req, res, next) {
     process(req.file.buffer)
     res.sendFile(__dirname+'/uploads/result.jpg')
 })
+
+async function getText(){
+    const apiURL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1'
+    const response = await fetch(apiURL)
+    const json = await response.json()
+    console.log(json)
+}
 
 async function process(buffer){
     Jimp.read(buffer)
@@ -25,7 +31,7 @@ async function process(buffer){
             image.write(__dirname+'/uploads/result.jpg')
         })
     })
-
+    getText()
 }
 
 app.listen(3021)
