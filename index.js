fd05d11af1ac1ec901d9bd36de5c85622a211248
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-09-07 15:10:28 -0500

    v2.2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-import fetch from 'node-fetch'
+const Request = require('request')
 const express = require('express')
 const app = express()
 const multer  = require('multer')
@@ -21,19 +20,20 @@ app.post('/process', upload.single('uploaded_file'), function (req, res, next) {
 
 async function process(buffer){
     const apiURL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1'
-    const response = await fetch(apiURL)
-    const json = await response.json()
-    const text = json.quotes[0].text
-    const author = json.quotes[0].author
-    Jimp.read(buffer)
-    .then(image => {
-        Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
-            const x = image.bitmap.width - (image.bitmap.width*0.99)
-            const yt = image.bitmap.height - (image.bitmap.height*0.30)
-            const ya = image.bitmap.height - (image.bitmap.height*0.20)
-            image.print(font, x, yt, text, (image.bitmap.width - (image.bitmap.width*0.10)))
-            image.print(font, x, ya, author, (image.bitmap.width - (image.bitmap.width*0.10)))
-            image.write(__dirname+'/uploads/result.jpg')
+    Request.get(apiURL, (error, response, body) => {        
+        var mess = JSON.parse(body)
+        const text = mess.quotes[0].text
+        const author = mess.quotes[0].author
+        Jimp.read(buffer)
+        .then(image => {
+            Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
+                const x = image.bitmap.width - (image.bitmap.width*0.99)
+                const yt = image.bitmap.height - (image.bitmap.height*0.30)
+                const ya = image.bitmap.height - (image.bitmap.height*0.20)
+                image.print(font, x, yt, text, (image.bitmap.width - (image.bitmap.width*0.10)))
+                image.print(font, x, ya, author, (image.bitmap.width - (image.bitmap.width*0.10)))
+                image.write(__dirname+'/uploads/result.jpg')
+            })
         })
     })
 }
