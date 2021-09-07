6a1575a70e59b968da56f95363af066c8077e5d1
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-09-07 03:20:29 -0500

    complete write

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const express = require('express')
 const app = express()
 const multer  = require('multer')
+const Jimp = require('jimp');
+const { text } = require('express');
 const upload = multer({ storage: multer.memoryStorage() })
-const Jimp = require('jimp')
-import fetch from 'node-fetch'
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname+'/src/index.html')
@@ -14,24 +15,20 @@ app.post('/stats', upload.single('uploaded_file'), function (req, res, next) {
     res.sendFile(__dirname+'/uploads/result.jpg')
 })
 
-async function getText(){
+async function process(buffer){
     const apiURL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1'
     const response = await fetch(apiURL)
     const json = await response.json()
-    console.log(json)
-}
-
-async function process(buffer){
+    const text = json.quotes[0].text
     Jimp.read(buffer)
     .then(image => {
         Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
             const x = image.bitmap.width - (image.bitmap.width*0.85)
             const y = image.bitmap.height - (image.bitmap.height*0.2)
-            image.print(font, x, y,"Hello world!")
+            image.print(font, x, y, text)
             image.write(__dirname+'/uploads/result.jpg')
         })
     })
-    getText()
 }
 
 app.listen(3021)
