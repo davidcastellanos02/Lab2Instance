3beeec8f1402f5dbfe85c58256733074b992fb71
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-09-07 09:22:34 -0500

    v2

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const express = require('express')
 const app = express()
 const multer  = require('multer')
-const Jimp = require('jimp');
-const { text } = require('express');
 const upload = multer({ storage: multer.memoryStorage() })
+const Jimp = require('jimp');
 
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname+'/src/index.html')
+    res.sendFile(__dirname+'/uploads/result.jpg')
 })
 
-app.post('/stats', upload.single('uploaded_file'), function (req, res, next) {
+app.post('/process', upload.single('uploaded_file'), function (req, res, next) {
+    try{
     process(req.file.buffer)
-    res.sendFile(__dirname+'/uploads/result.jpg')
+    res.sendFile(__dirname+'/src/result.html')
+    }catch(err){
+        
+    }
 })
 
 async function process(buffer){
@@ -20,12 +23,15 @@ async function process(buffer){
     const response = await fetch(apiURL)
     const json = await response.json()
     const text = json.quotes[0].text
+    const author = json.quotes[0].author
     Jimp.read(buffer)
     .then(image => {
         Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
-            const x = image.bitmap.width - (image.bitmap.width*0.85)
-            const y = image.bitmap.height - (image.bitmap.height*0.2)
-            image.print(font, x, y, text)
+            const x = image.bitmap.width - (image.bitmap.width*0.99)
+            const yt = image.bitmap.height - (image.bitmap.height*0.30)
+            const ya = image.bitmap.height - (image.bitmap.height*0.20)
+            image.print(font, x, yt, text, (image.bitmap.width - (image.bitmap.width*0.10)))
+            image.print(font, x, ya, author, (image.bitmap.width - (image.bitmap.width*0.10)))
             image.write(__dirname+'/uploads/result.jpg')
         })
     })
